4e76167569e06a0c3dd60641111cae145a4708a7
xyuuu <songke0905@163.com>
2025-07-12 21:39:33 +0800

    Unit test successful

--- v1/main.cc
+++ v1/main.cc
@@ -11,8 +11,9 @@
 #include <fstream>
 #include <wfrest/HttpServer.h>
 #include <vector>
-
-
+#include <openssl/evp.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "CryptoUtil.h"
 #include "nlohmann/json.hpp"
 
@@ -42,6 +43,36 @@ struct Context {
     int id;
 };
 
+
+
+
+
+
+std::string sha256_hash(const char *data) {
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    if (!ctx) {
+        throw std::runtime_error("Failed to create EVP_MD_CTX");
+    }
+
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hash_len;
+    
+    // 初始化、更新和完成哈希计算
+    EVP_DigestInit_ex(ctx, EVP_sha256(), NULL);
+    EVP_DigestUpdate(ctx, data, strlen(data));
+    EVP_DigestFinal_ex(ctx, hash, &hash_len);
+    
+    EVP_MD_CTX_free(ctx);
+
+    // 将哈希值转换为十六进制字符串
+    std::stringstream ss;
+    for (unsigned int i = 0; i < hash_len; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    
+    return ss.str();
+}
+
 static WFFacilities::WaitGroup global_wait_group{1};
 
 class MyRouter {
@@ -67,6 +98,14 @@ public:
             this->search_file_callback(req, resp, series);
         });
 
+        // 文件上传
+        m_server.POST("/file/upload",[this](const HttpReq* req,HttpResp *resp,SeriesWork* series) {
+            this->upload_callback(req, resp, series);
+        });
+        // 文件下载
+        m_server.GET("/file/download",[this](const HttpReq* req,HttpResp *resp,SeriesWork* series) {
+            this->download_callback(req, resp, series);
+        });
     }
 
 private:
@@ -119,7 +158,6 @@ void register_callback(const HttpReq* req, HttpResp *resp, SeriesWork *series) {
 // 登录函数实现
 
 void login_callback(const HttpReq* req, HttpResp *resp,SeriesWork *series){
-    cout << "login_callback called" << endl;
     auto & form = req->form_kv();
     auto username_it = form.find("username");
     auto password_it = form.find("password");
@@ -159,11 +197,8 @@ void login_callback(const HttpReq* req, HttpResp *resp,SeriesWork *series){
             
             string db_hashcode = record[2].as_string(); 
             string db_salt = record[3].as_string();
-            cout << "db_salt: " <<db_salt<<endl;
-            cout << "db_hashcode: " << db_hashcode<<endl;
             string temp = ctx->password;     
             string gen_hashcode = CryptoUtil::hash_password(temp, db_salt);
-            cout << "gen_hashcode :" << gen_hashcode << endl;
             if (gen_hashcode == db_hashcode) {
                 resp->set_status(200);
                 string token = CryptoUtil::generate_token(ctx->username);
@@ -302,8 +337,162 @@ void search_file_callback(const HttpReq* req, HttpResp *resp,SeriesWork *series)
     });
     sql_task->get_req()->set_query(sql);
     series->push_back(sql_task);
-
 }
+
+// 上传文件
+void upload_callback(const HttpReq *req, HttpResp* resp, SeriesWork *series) {
+    string username = req->query("username");
+    const string & token = req->query("token");
+
+    if (!CryptoUtil::verify_token(token, username)) {
+        resp->set_status_code("401"); 
+        resp->append_output_body_nocopy("<html>401 Unauthorized</html>");
+        return;
+    }
+
+    if (req->content_type() != MULTIPART_FORM_DATA) {
+        resp->set_status(HttpStatusBadRequest);     
+        return;
+    }
+
+    // 查询用户 UID
+    string sql_user = "SELECT * FROM tbl_user WHERE username = '" + username + "'";
+    WFMySQLTask *sql_task = WFTaskFactory::create_mysql_task(MYSQL_URL, MAX_RETRY,
+        [req, resp, series,username](WFMySQLTask *sql_task) {
+            if (sql_task->get_state() != WFT_STATE_SUCCESS) {
+                resp->set_status(500);
+                resp->String("Database error!");
+                return;
+            }
+
+            MySQLResultCursor cursor{sql_task->get_resp()};
+            std::vector<MySQLCell> record;
+            if (!cursor.fetch_row(record)) {
+                resp->set_status(401);
+                resp->String("Invalid username");
+                return;
+            }
+
+            int uid = record[0].as_int();
+            const Form& form = req->form();
+
+            for (const auto& [_, file] : form) {
+                
+                const auto& [filename, content] = file;
+                string hashcode = sha256_hash(content.c_str());
+                int filesize = content.size();
+                string dir_path = "./disk/" + username;
+                string save_path = dir_path + "/" + hashcode;
+                struct stat st;
+                if (stat("./disk", &st) != 0) {
+                    mkdir("./disk", 0755);
+                }
+                
+                if (stat(dir_path.c_str(), &st) != 0) {
+                    mkdir(dir_path.c_str(), 0755);
+                }
+                resp->Save(save_path, content);
+                std::string sql_insert = "INSERT INTO tbl_file(uid, filename, hashcode, size) VALUES (" +
+                                         std::to_string(uid) + ", '" +
+                                         filename + "', '" +
+                                         hashcode + "', " +
+                                         std::to_string(filesize) + ")";
+
+                WFMySQLTask* insert_task = WFTaskFactory::create_mysql_task(MYSQL_URL, MAX_RETRY,
+                    [filename, resp](WFMySQLTask* task) {
+                        if (task->get_state() != WFT_STATE_SUCCESS) {
+                            resp->set_status(500);
+                            resp->String("Database error!");
+                            return;
+                        }
+                        resp->set_status(200);
+                        resp->String("Save " + string(filename) + " success!\n");
+                    });
+
+                insert_task->get_req()->set_query(sql_insert);
+                series->push_back(insert_task);
+            }
+        });
+
+    sql_task->get_req()->set_query(sql_user);
+    series->push_back(sql_task);
+}
+
+
+
+
+void download_callback(const HttpReq *req, HttpResp *resp, SeriesWork *series) {
+    std::string username = req->query("username");
+    const std::string &token = req->query("token");
+    std::string hashcode = req->query("hashcode");
+    std::string filename = req->query("filename");
+
+    if (!CryptoUtil::verify_token(token, username)) {
+        resp->set_status_code("401"); 
+        resp->append_output_body_nocopy("<html>401 Unauthorized</html>");
+        return;
+    }
+
+    std::string sql_user = "SELECT * FROM tbl_user WHERE username = '" + username + "'";
+    WFMySQLTask *sql_task = WFTaskFactory::create_mysql_task(MYSQL_URL, MAX_RETRY,
+        [req, resp, series, hashcode, filename,username](WFMySQLTask *sql_task) {
+            if (sql_task->get_state() != WFT_STATE_SUCCESS) {
+                resp->set_status(500);
+                resp->String("Database error!");
+                return;
+            }
+
+            MySQLResultCursor cursor{sql_task->get_resp()};
+            std::vector<MySQLCell> record;
+            if (!cursor.fetch_row(record)) {
+                resp->set_status(401);
+                resp->String("Invalid username");
+                return;
+            }
+            if(record[2].as_string() != filename) {
+                resp->set_status(400);
+                resp->String("Filename mismatch");
+                return;
+            }
+            int uid = record[0].as_int();
+            std::string sql2 = "SELECT * FROM tbl_file WHERE uid = " + std::to_string(uid) +
+                               " AND hashcode = '" + hashcode + "'";
+
+            WFMySQLTask* select_task = WFTaskFactory::create_mysql_task(MYSQL_URL, MAX_RETRY,
+                [resp, filename,username,hashcode](WFMySQLTask* task) {
+                    if (task->get_state() != WFT_STATE_SUCCESS) {
+                        resp->set_status(500);
+                        resp->String("Database error!");
+                        return;
+                    }
+
+                    MySQLResultCursor cursor{task->get_resp()};
+                    std::vector<MySQLCell> record;
+                    if (!cursor.fetch_row(record)) {
+                        resp->set_status(404);
+                        resp->String("File not found");
+                        return;
+                    }
+                    // 构造保存路径
+                    string filepath = "./disk/" + username + "/" + hashcode;
+                    struct stat st;
+                    if (stat(filepath.c_str(), &st) != 0) {
+                        resp->set_status(404);
+                        resp->String("File missing on server");
+                        return;
+                    }
+                    resp->set_status(200);
+                    resp->File(filepath);               
+                });
+
+            select_task->get_req()->set_query(sql2);
+            series->push_back(select_task);
+        });
+
+    sql_task->get_req()->set_query(sql_user);
+    series->push_back(sql_task);
+}
+
     HttpServer& m_server;
 };
 
