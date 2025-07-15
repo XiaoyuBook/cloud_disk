1c85e9eb3c400f7ada4219793ae96a70693da3eb
xyuuu <songke0905@163.com>
2025-07-15 20:33:43 +0800

    RabbitMQ unit test successfully

---
+++ v3/RabbitMQ.cc
@@ -0,0 +1,50 @@
+#include "RabbitMQ.h"
+
+using json = nlohmann::json;
+using std::string;
+
+
+RabbitMQ::RabbitMQ(const std::string& config_path) {
+    get_RabbitMQConfig(config_path);
+}
+
+
+void RabbitMQ::get_RabbitMQConfig(const std::string& config_path) {
+        std::ifstream ifs(config_path);  
+        if (!ifs.is_open()) {
+            throw std::runtime_error("无法打开配置文件: " + config_path);
+        }
+
+        json config = json::parse(ifs); 
+        m_host = config["host"];
+        m_port = config["port"];
+        m_username = config["username"];
+        m_password = config["password"];
+        m_vhost = config["vhost"];
+        m_exchange = config["exchange"];
+        m_routing_key = config["routing_key"];
+        m_uri = config["uri"];
+        m_queue = config["queue"];
+}
+
+void RabbitMQ::producer(const char * content) {
+    AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create(m_host,m_port,m_username,m_password,m_vhost);
+    AmqpClient::BasicMessage::ptr_t message = AmqpClient::BasicMessage::Create(content);
+    channel->BasicPublish(m_exchange, m_routing_key, message);
+}
+
+
+void RabbitMQ::consumer() {
+    AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::CreateFromUri(m_uri);
+    AmqpClient::Envelope::ptr_t envelope;
+    const string &q = m_queue;
+    channel->BasicConsume(q);
+    while(1) {
+        AmqpClient::Envelope::ptr_t envelope = channel->BasicConsumeMessage();
+        if(envelope && envelope->Message()) {
+            std::cout << envelope->Message()->Body() << std::endl;
+        }
+    }
+}
+
+
