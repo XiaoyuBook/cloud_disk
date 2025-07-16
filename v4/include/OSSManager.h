1179c49cb68d8df924c0839af4a3a0bc447019f5
xyuuu <songke0905@163.com>
2025-07-16 22:34:27 +0800

    v4_finished

---
+++ v4/include/OSSManager.h
@@ -0,0 +1,30 @@
+#ifndef OSS_MANAGER_H
+#define OSS_MANAGER_H
+
+#include <string>
+#include <memory>
+#include <fstream>
+#include <alibabacloud/oss/OssClient.h>
+#include "nlohmann/json.hpp"
+
+#define OSS_CONFIG_PATH "../config/OSS_config.json"
+
+class OSSManager {
+public:
+    explicit OSSManager(const char* config_path);
+    ~OSSManager();
+
+    bool upload_file(const std::string& bucketName,
+                    const std::string& objectName,
+                    const std::string& localFilePath);
+
+private:
+    void get_OSSManager_config(const char* config_path);
+
+    std::string m_endpoint;
+    std::string m_accessKeyId;
+    std::string m_accessKeySecret;
+    std::unique_ptr<AlibabaCloud::OSS::OssClient> m_client;
+};
+
+#endif
---
+++ v4/src/RabbitMQ.cc
@@ -0,0 +1,61 @@
+#include "../include/RabbitMQ.h"
+#include "../include/OSSManager.h"
+using json = nlohmann::json;
+using std::string;
+
+
+RabbitMQ::RabbitMQ(const std::string& config_path) {
+    get_RabbitMQConfig(config_path);
+}
+
+
+void RabbitMQ::get_RabbitMQConfig(const std::string& config_path) {
+        std::ifstream ifs(config_path);  
+        if (!ifs.is_open()) {
+            std::cout << "无法打开配置文件" << std::endl;
+            return;
+        }
+
+        json config = json::parse(ifs); 
+        m_host = config["host"];
+        m_port = config["port"];
+        m_username = config["username"];
+        m_password = config["password"];
+        m_vhost = config["vhost"];
+        m_exchange = config["exchange"];
+        m_routing_key = config["routing_key"];
+        m_uri = config["uri"];
+        m_queue = config["queue"];
+}
+
+void RabbitMQ::producer(const char * content) {
+    AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create(m_host,m_port,m_username,m_password,m_vhost);
+    AmqpClient::BasicMessage::ptr_t message = AmqpClient::BasicMessage::Create(content);
+    channel->BasicPublish(m_exchange, m_routing_key, message);
+}
+
+
+void RabbitMQ::consumer() {
+    AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::CreateFromUri(m_uri);
+    AmqpClient::Envelope::ptr_t envelope;
+    const string &q = m_queue;
+    channel->BasicConsume(q);
+    while(1) {
+        AmqpClient::Envelope::ptr_t envelope = channel->BasicConsumeMessage();
+        if(envelope && envelope->Message()) {
+            sleep(1);
+            string objectName = envelope->Message()->Body();
+            OSSManager oss(OSS_CONFIG_PATH);
+            string bucketName = "oss-learing";
+            string filePath = string("../")+objectName; 
+            std::cout << "filePath :" << filePath << std::endl;
+            std::cout << "objectName: "  << objectName << std::endl;
+            bool ok = oss.upload_file(bucketName, objectName, filePath);
+            if(!ok) {
+                std::cout << "failed to upload oss" << std::endl;
+            }
+        }
+    }
+}
+
+
